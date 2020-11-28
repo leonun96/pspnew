84712c271904e4d712d7051117edcd846ffa6e6d
leandro96x <46700574+leandro-chile@users.noreply.github.com>
2020-11-28 18:23:36 -0300

    Clases faltantes a los controladores.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,11 @@ use App\Models\Niveles;
 use App\Models\Profesores;
 use App\Models\Categorias;
 use App\Models\Subcategorias;
+use App\Models\Alumnos;
+use App\Models\Actividades;
+use App\Models\Preguntas;
+use App\Models\Respuestas;
+use Flash;
 
 class HomeController extends Controller
 {
--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -3,6 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Niveles;
+use App\Models\Profesores;
+use App\Models\Categorias;
+use App\Models\Subcategorias;
+use App\Models\Alumnos;
+use App\Models\Actividades;
+use App\Models\Preguntas;
+use App\Models\Respuestas;
+use Flash;
 
 class ProfesorController extends Controller
 {
