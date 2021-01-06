5bc2ff003138f5bb6e39e744509d2838485b7755
leandro96x <46700574+leandro-chile@users.noreply.github.com>
2021-01-06 01:25:19 -0300

    Cambios migraciones. Cambio de api auth. Se añaden api tokens

--- database/migrations/2020_11_28_163050_create_profesores_table.php
+++ database/migrations/2020_11_28_163050_create_profesores_table.php
@@ -6,34 +6,35 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateProfesoresTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('profesores', function (Blueprint $table) {
-            $table->id();
-            $table->string('rut')->unique()->nullable();
-            $table->string('nombres')->nullable();
-            $table->string('apellidos')->nullable();
-            $table->date('fnac')->nullable();
-            $table->integer('telefono')->nullable();
-            $table->string('email')->nullable();
-            $table->string('password')->nullable();
-            $table->softDeletes();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('profesores', function (Blueprint $table) {
+			$table->id();
+			$table->string('rut')->unique()->nullable();
+			$table->string('nombres')->nullable();
+			$table->string('apellidos')->nullable();
+			$table->date('fnac')->nullable();
+			$table->integer('telefono')->nullable();
+			$table->string('email')->nullable();
+			$table->string('password')->nullable();
+			$table->string('api_token')->nullable();
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('profesores');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('profesores');
+	}
 }
--- database/migrations/2020_11_28_163240_create_alumnos_table.php
+++ database/migrations/2020_11_28_163240_create_alumnos_table.php
@@ -6,37 +6,38 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateAlumnosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('alumnos', function (Blueprint $table) {
-            $table->id();
-            $table->string('rut')->unique()->nullable();
-            $table->string('nombres')->nullable();
-            $table->string('apellidos')->nullable();
-            $table->date('fnac')->nullable();
-            $table->string('telefono')->nullable();
-            $table->string('email')->nullable();
-            $table->string('password')->nullable();
-            $table->string('NEE')->nullable();
-            $table->unsignedBigInteger('profesores_id')->nullable();
-            $table->foreign('profesores_id')->references('id')->on('profesores');
-            $table->softDeletes();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('alumnos', function (Blueprint $table) {
+			$table->id();
+			$table->string('rut')->unique()->nullable();
+			$table->string('nombres')->nullable();
+			$table->string('apellidos')->nullable();
+			$table->date('fnac')->nullable();
+			$table->string('telefono')->nullable();
+			$table->string('email')->nullable();
+			$table->string('password')->nullable();
+			$table->string('api_token')->nullable();
+			$table->string('NEE')->nullable();
+			$table->unsignedBigInteger('profesores_id')->nullable();
+			$table->foreign('profesores_id')->references('id')->on('profesores');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('alumnos');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('alumnos');
+	}
 }
