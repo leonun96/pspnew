2a4224bc0d0310b5730097b86eb43eb0961d9407
leandro96x <46700574+leandro-chile@users.noreply.github.com>
2021-01-08 21:29:53 -0300

    Correccion para borrar actividad. Correccion para ver actividades borradas.

--- resources/views/alumnos/ver/actividades.blade.php
+++ resources/views/alumnos/ver/actividades.blade.php
@@ -6,32 +6,34 @@
 <h1 class="text-center uppercase text-bold">Listado de actividades</h1>
 
 <div class="row col-12">
-    <table class="table purple">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Actividad</th>
-            <th scope="col">Categoria</th>
-            <th scope="col">Fecha inicio</th>
-            <th scope="col">Fecha termino</th>
-            <th scope="col">Tiempo para realizarla</th>
-            <th scope="col">Estado</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach ($actividades as $actividad)
-          <tr>
-            <th scope="row">{{ $actividad->id }}</th>
-            <td><a href="{{ Route('alumno.realizarAct',$actividad->id) }}">{{ $actividad->actividades->nombre }}</a></td>
-            <td>{{$actividad->actividades->subcategorias->categorias->nombre }}</td>
-            <td>{{ $actividad->fecha_inicio }}</td>
-            <td>{{ $actividad->fecha_termino }}</td>
-            <td>{{ $actividad->tiempo }}</td>
-            <td >{{ $actividad->estado }}</td>
-          </tr>
-            @endforeach
-        </tbody>
-      </table>
+	<table class="table purple">
+		<thead class="thead-dark">
+			<tr>
+				<th scope="col">#</th>
+				<th scope="col">Actividad</th>
+				<th scope="col">Categoria</th>
+				<th scope="col">Fecha inicio</th>
+				<th scope="col">Fecha termino</th>
+				<th scope="col">Tiempo para realizarla</th>
+				<th scope="col">Estado</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($actividades as $actividad)
+			@if(!is_null($actividad->actividades))
+			<tr>
+				<th scope="row">{{ $actividad->id }}</th>
+				<td><a href="{{ Route('alumno.realizarAct',$actividad->id) }}">{{ $actividad->actividades->nombre }}</a></td>
+				<td>{{$actividad->actividades->subcategorias->categorias->nombre }}</td>
+				<td>{{ $actividad->fecha_inicio }}</td>
+				<td>{{ $actividad->fecha_termino }}</td>
+				<td>{{ $actividad->tiempo }}</td>
+				<td >{{ $actividad->estado }}</td>
+			</tr>
+			@endif
+			@endforeach
+		</tbody>
+	</table>
 </div>
 		
 @endsection
