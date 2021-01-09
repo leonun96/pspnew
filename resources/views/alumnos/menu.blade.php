9ff133bcf16144452194599321fcdb9469d42bf9
Leandro Núñez <leandro161996@gmail.com>
2021-01-09 01:54:42 -0300

    - Boton quitar modal arreglado.

--- resources/views/alumnos/menu.blade.php
+++ resources/views/alumnos/menu.blade.php
@@ -30,11 +30,11 @@
 		</div>
 	</div>
 
-	<div class="col-12">
+	{{-- <div class="col-12">
 		<button type="button" class="btn btn-warning  btn-block" data-toggle="modal" data-target="#exampleModal">
 			<i class="fas fa-envelope-open-text"></i> Enviar Correo al profesor
 		</button>
-	</div>
+	</div> --}}
 
 
 
@@ -43,27 +43,30 @@
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
-	  <div class="modal-content">
-		<div class="modal-header">
-		  <h5 class="modal-title" id="exampleModalLabel">Enviar Correo</h5>
-		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Enviar Correo</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<form action="" method="post">
+				@csrf
+				<div class="modal-body">
+					<div class="mb-3">
+						<label for="exampleFormControlInput1" class="form-label">Asunto</label>
+						<input type="email" class="form-control" id="exampleFormControlInput1">
+					</div>
+					<div class="mb-3">
+						<label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
+						<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+					<button type="button" class="btn btn-primary">Enviar</button>
+				</div>
+			</form>
 		</div>
-		<div class="modal-body">
-			<div class="mb-3">
-				<label for="exampleFormControlInput1" class="form-label">Asunto</label>
-				<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-			  </div>
-			  <div class="mb-3">
-				<label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
-				<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-			  </div>
-		</div>
-		<div class="modal-footer">
-		  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-		  <button type="button" class="btn btn-primary">Enviar</button>
-		</div>
-	  </div>
 	</div>
-  </div>
+</div>
 		
 @endsection
