339e09cfa05e248aa0f2cb08cdb6b3130089b5b4
Leandro Núñez <leandro161996@gmail.com>
2021-01-09 01:49:35 -0300

    Navegar al menu al apretar imagen y boton logo.

--- resources/views/layouts/alumno.blade.php
+++ resources/views/layouts/alumno.blade.php
@@ -27,7 +27,7 @@
 	<!-- Main Sidebar Container -->
 	<aside class="main-sidebar sidebar-dark-purple elevation-4">
 		<!-- Brand Logo -->
-		<a href="" class="brand-link">
+		<a href="{{ route('alumno.menu') }}" class="brand-link">
 			<img src="{{ asset('AdminLte/img/AdminLTELogo.png') }}" alt="" class="brand-image img-circle elevation-3" style="opacity: .8">
 			<span class="brand-text font-weight-light">PSP SYSTEM</span>
 		</a>
@@ -39,7 +39,7 @@
 					<img src="{{ asset('AdminLte/img/avatar.jpg') }}" class="img-circle elevation-2" alt="User Image">
 				</div>
 				<div class="info">
-					<a href="" class="d-block">{{ Auth::guard('alumno')->user()->nombres }} {{ Auth::guard('alumno')->user()->apellidos }}</a>
+					<a href="{{ route('alumno.menu') }}" class="d-block">{{ Auth::guard('alumno')->user()->nombres }} {{ Auth::guard('alumno')->user()->apellidos }}</a>
 				</div>
 			</div>
 			<!-- Sidebar Menu -->
